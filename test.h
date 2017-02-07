5d7389ae15d987fbbd3518cd750650991b12581e
Jos Qiao <13267065761@163.com>
2017-02-07 15:13:18 +0800

    add test file

---
+++ test.h
@@ -0,0 +1,37 @@
+//
+//  ViewController.h
+//  TYDownloadManagerDemo
+//
+//  Created by tany on 16/6/12.
+//  Copyright © 2016年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DownloadDataViewController : UIViewController
+
+
+
+
+@end
+
+
+@implementation DownloadDataViewController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        char *a = "abacadfadfadfasf";
+        
+        char *c = "git is free software distributed under the GPL";
+        
+        char *b = "Git has a mutable index called stage";
+        
+        char *d = "git add a license file";
+    }
+    return self;
+}
+
+@end
